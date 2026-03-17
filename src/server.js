bd993ec6facdb64c3d85da3bf685a10c96adde14
lunuarovich <lenuar.max@gmail.com>
2026-03-17 14:27:24 +0200

    add server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,52 @@
-const message = 'Hello world';
+require('dotenv').config();
 
-console.log(message);
+const express = require('express');
+const cors = require('cors');
+const pinoHttp = require('pino-http');
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  pinoHttp({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
